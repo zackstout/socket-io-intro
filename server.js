4881f6a2d19750d56e911892738f6402150bf9fc
Zack Stout <zackstout@gmail.com>
2018-02-09 19:35:42 -0600

    basic chat app working

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  // res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Differs from Ratchet in that you can only open up the one server, but see it in multiple tabs/browsers.
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
